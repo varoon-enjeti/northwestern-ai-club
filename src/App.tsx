4e666f057a1a80f6685c424239897f32672b1409
varoon-enjeti <varoon.enjeti@gmail.com>
2023-08-07 00:55:05 -0500

    working on the sign up sheet

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Signup from './signup';
 
 function App() {
   return (
-    <div className="App flex flex-col items-center">
+    <div className="App flex flex-col items-center w-screen h-screen">
       <div className="text-3xl text-nupurple">Northwestern AI</div>
       <Signup />
     </div>
--- src/signup.tsx
+++ src/signup.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 export default function Signup() {
     return (
-        <div className="flex justify-center w-full">
-            <form className="flex-col justify-center content-center w-1/3" action="">
+        <div className="flex justify-center w-full h-full">
+            <form className="flex flex-col content-center gap-2 w-1/3 h-full" action="">
                 <p className="font-sans">First Name</p>
-                <input className="bg-gray-200 rounded w-full h-10" type="text"/>
+                <input className="bg-gray-200 rounded w-full h-10" type="text" required/>
                 <p className="">Last Name</p>
-                <input className="bg-gray-200 rounded w-full h-10" type="text"/>
+                <input className="bg-gray-200 rounded w-full h-10" type="text" required/>
                 <p className="">Northwestern Email</p>
-                <input className="bg-gray-200 rounded w-full h-10" type="text"/>
+                <input className="bg-gray-200 rounded w-full h-10" type="text" required/>
                 <p className="">Major</p>
-                <input className="bg-gray-200 rounded w-full h-10" type="text"/>
+                <input className="bg-gray-200 rounded w-full h-10" type="text" required/>
                 <p className="">Graduation Year</p>
-                <select className="bg-gray-200 rounded w-full h-10" name="year" id="">
+                <select className="bg-gray-200 rounded w-full h-10" name="year" id="" required>
                     <option value="" disabled selected>-- select a year --</option>
                     <option value="2024">2024</option>
                     <option value="2025">2025</option>
@@ -21,7 +21,7 @@ export default function Signup() {
                     <option value="2027">2027</option>
                     <option value="Other">Other</option>
                 </select>
-                <input className='bg-gray-300 rounded' type="submit" value="Join"/>
+                <input className='bg-nupurple text-white rounded w-1/3 self-center hover:bg-purple-800' type="submit" value="Join"/>
             </form>
         </div>
     );
