ae8d6e8454a97c684cb0d47b6b0ff9cbd44cc691
varoon-enjeti <varoon.enjeti@gmail.com>
2023-08-18 02:58:11 -0500

    tracking-tight

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -5,9 +5,9 @@ export default function LandingPage() {
         <div className="w-full h-full bg-[url('/texture.svg')]">
             <div className="h-full w-full flex justify-center overflow-hidden pl-20 items-center bg-gradient-to-b from-white via-transparent via-40% to-white">
                 <div className="flex flex-col gap-3">
-                    <p className='h-24 -ml-1 -mb-3 text-8xl tracking-tighter font-sans font-bold text-transparent bg-clip-text bg-gradient-to-b from-nupurple via-nupurple via-65%'>Northwestern AI</p>
-                    <p className="w-2/3 text-2xl tracking-tighter font-sans font-medium text-nulight">Educating and Empowering Students to Shape the Future with Artificial Intelligence.</p>
-                    <a href="#" className='w-1/6 h-10 flex items-center justify-center font-sans tracking-tighter font-medium text-xl bg-nupurple rounded-lg text-white hover:scale-105 duration-200'>Sign Up</a>
+                    <p className='h-24 -ml-1 -mb-3 text-8xl tracking-tight font-sans font-bold text-transparent bg-clip-text bg-gradient-to-b from-nupurple via-nupurple via-65%'>Northwestern AI</p>
+                    <p className="w-2/3 text-2xl font-sans font-normal tracking-tight text-nulight">Educating and Empowering Students to Shape the Future with Artificial Intelligence.</p>
+                    <a href="#" className='w-1/6 h-10 flex items-center justify-center font-sans tracking-tight font-medium text-xl bg-nupurple rounded-lg text-white hover:scale-105 duration-200'>Sign Up</a>
                 </div>
                 <iframe className='-mr-80 w-[52%] h-[135%] ml-auto rotate-[-17deg]' src="https://particles-js-gamma.vercel.app"></iframe>
             </div>
