95af9c69c613a917fef0a2e83975e77576b234df
varoon-enjeti <varoon.enjeti@gmail.com>
2023-08-18 17:39:57 -0500

    working on header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Header from '@/components/header'
 import LandingPage from '@/components/landing-page'
 import About from '@/components/about'
 
 export default function Home() {
   return (
     <main className="h-screen">
+      <Header />
       <LandingPage />
       <About />
     </main>
---
+++ components/header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return (
+       <header className="h-16 text-7xl fixed">hello</header>
+    );
+}
