7aae652e3ffdb23a85041664b37b134678bbf723
varoon-enjeti <varoon.enjeti@gmail.com>
2023-08-18 02:38:13 -0500

    done for the night

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import LandingPage from '@/components/landing-page'
+import About from '@/components/about'
 
 export default function Home() {
   return (
     <main className="h-screen">
       <LandingPage />
+      <About />
     </main>
   )
 }
