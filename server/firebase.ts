c8996d409a612b90547872187686ccf71ab5e942
varoon-enjeti <varoon.enjeti@gmail.com>
2023-08-17 23:27:31 -0500

    everything should be ready now

---
+++ server/firebase.ts
@@ -0,0 +1,34 @@
+import { initializeApp, deleteApp } from "firebase/app";
+import { getFirestore, collection, doc, setDoc } from "firebase/firestore";
+import { exit } from "process";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRBNdSciIhOheTbcPr8WJHZoNyjM2y6Mo",
+  authDomain: "northwestern-ai-website-3b846.firebaseapp.com",
+  projectId: "northwestern-ai-website-3b846",
+  storageBucket: "northwestern-ai-website-3b846.appspot.com",
+  messagingSenderId: "157698718705",
+  appId: "1:157698718705:web:d0e946befe26379927a250",
+  measurementId: "G-MQHVCXC1KX"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+ export async function newMember(firstName: string, lastName: string, email: string, netid: string, major: string, year: string){
+  try {
+    await setDoc(doc(db, "users", netid), {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      major: major,
+      year: year
+    });
+    console.log("New user " + netid + " added.");
+  } catch (e) {
+    console.error("Error adding new user " + netid + ".");
+    console.log(e);
+  } finally {
+        console.log("Done");
+    }
+}
