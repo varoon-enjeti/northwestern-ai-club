2de6728582a446f16a0998a78d3dae177ff08c7a
varoon-enjeti <varoon.enjeti@gmail.com>
2023-08-19 16:37:15 -0500

    header

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,25 @@
+import Link from 'next/link'
+import About from './about';
+
 export default function Header() {
     return (
-       <header className="h-16 text-7xl fixed">hello</header>
+       <header className="h-16 w-2/3 fixed flex flex-row text-center gap-96">
+            <div className='flex flex-row gap-10 text-center items-center'>
+                <Link href="/" className='mr-10'>
+                    <img src="nuai.png" className='h-16 w-16'/>
+                </Link>
+                <Link href="/" className='font-sans text-nupurple text-lg hover:text-nulight'>About</Link>
+                <Link href="/" className='font-sans text-nupurple text-lg hover:text-nulight'>Events</Link>
+                <Link href="/" className='font-sans text-nupurple text-lg hover:text-nulight'>Team</Link>
+            </div>
+            <div className='flex flex-row items-center'>
+                <Link href="https://www.instagram.com/northwestern_ai/" target="_blank" className='mr-10'>
+                    <img src="instagram.svg" className='h-6 w-6'/>
+                </Link>
+                <Link href="https://www.linkedin.com/groups/14288307/" target="_blank" className='mr-10'>
+                    <img src="linkedin.svg" className='h-6 w-6'/>
+                </Link>
+            </div>
+       </header>
     );
 }
