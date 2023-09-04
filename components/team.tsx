df4e504b3d484a3c82e7baabd776b5c013672805
varoon-enjeti <varoon.enjeti@gmail.com>
2023-09-04 14:44:15 +0200

    added to teams

--- components/team.tsx
+++ components/team.tsx
@@ -4,41 +4,41 @@ export default function Team() {
             <p className="font-sans font-bold text-5xl text-nupurple tracking-tight mt-[6rem]">Meet the Team!</p>
             <div className="flex flex-row gap-20">
                 <div className="flex flex-col items-center">
-                    <a href="" className="hover:scale-105 duration-200" target="_blank">
-                        <img src="varoon.png" className="h-48 w-48 object-cover rounded-full border-4 border-gray-400" />
+                    <a href="https://www.linkedin.com/feed/" className="hover:scale-105 duration-200" target="_blank">
+                        <img src="nuai.png" className="h-48 w-48 object-cover rounded-full border-4 border-gray-400" />
                     </a>
-                    <p className="font-sans font-medium text-lg tracking-tight mt-3">Varoon Enjeti</p>
-                    <p className="font-sans font-light tracking-tight">President</p>
+                    <p className="font-sans font-medium text-lg tracking-tight mt-3">Isaac ("Big Papi") Miller</p>
+                    <p className="font-sans font-light tracking-tight">Co-President</p>
                 </div>
                 <div className="flex flex-col items-center">
-                    <a href="" className="hover:scale-105 duration-200" target="_blank">
-                        <img src="varoon.png" className="h-48 w-48 object-cover rounded-full border-4 border-gray-400" />
+                    <a href="https://www.linkedin.com/feed/" className="hover:scale-105 duration-200" target="_blank">
+                        <img src="nuai.png" className="h-48 w-48 object-cover rounded-full border-4 border-gray-400" />
                     </a>
-                    <p className="font-sans font-medium text-lg tracking-tight mt-3">Varoon Enjeti</p>
-                    <p className="font-sans font-light tracking-tight">President</p>
+                    <p className="font-sans font-medium text-lg tracking-tight mt-3">James Kuntz</p>
+                    <p className="font-sans font-light tracking-tight">Co-President</p>
                 </div>
             </div>
             <div className="flex flex-row gap-20">
                 <div className="flex flex-col items-center">
-                    <a href="" className="hover:scale-105 duration-200" target="_blank">
-                        <img src="varoon.png" className="h-48 w-48 object-cover rounded-full border-4 border-gray-400" />
+                    <a href="https://www.linkedin.com/feed/" className="hover:scale-105 duration-200" target="_blank">
+                        <img src="nuai.png" className="h-48 w-48 object-cover rounded-full border-4 border-gray-400" />
                     </a>
-                    <p className="font-sans font-medium text-lg tracking-tight mt-3">Varoon Enjeti</p>
-                    <p className="font-sans font-light tracking-tight">President</p>
+                    <p className="font-sans font-medium text-lg tracking-tight mt-3">Grace Downing</p>
+                    <p className="font-sans font-light tracking-tight">Executive Member</p>
                 </div>
                 <div className="flex flex-col items-center">
-                    <a href="" className="hover:scale-105 duration-200" target="_blank">
-                        <img src="varoon.png" className="h-48 w-48 object-cover rounded-full border-4 border-gray-400" />
+                    <a href="https://www.linkedin.com/feed/" className="hover:scale-105 duration-200" target="_blank">
+                        <img src="nuai.png" className="h-48 w-48 object-cover rounded-full border-4 border-gray-400" />
                     </a>
-                    <p className="font-sans font-medium text-lg tracking-tight mt-3">Varoon Enjeti</p>
-                    <p className="font-sans font-light tracking-tight">President</p>
+                    <p className="font-sans font-medium text-lg tracking-tight mt-3">Helena Guajardo</p>
+                    <p className="font-sans font-light tracking-tight">Executive Member</p>
                 </div>
                 <div className="flex flex-col items-center">
-                    <a href="" className="hover:scale-105 duration-200" target="_blank">
+                    <a href="https://www.linkedin.com/in/varoonenjeti/" className="hover:scale-105 duration-200" target="_blank">
                         <img src="varoon.png" className="h-48 w-48 object-cover rounded-full border-4 border-gray-400" />
                     </a>
                     <p className="font-sans font-medium text-lg tracking-tight mt-3">Varoon Enjeti</p>
-                    <p className="font-sans font-light tracking-tight">President</p>
+                    <p className="font-sans font-light tracking-tight">Executive Member</p>
                 </div>
             </div>
         </div>
