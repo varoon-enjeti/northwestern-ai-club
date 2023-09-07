4e9fdf6564f11d81d8e75ece74bb9745f2f15adb
varoon-enjeti <varoon.enjeti@gmail.com>
2023-09-07 15:46:31 +0200

    events page done

---
+++ components/eventcard.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+
+interface Card {
+    title: string;
+    text: string;
+    image: string;
+    id: string;
+}
+
+export default function Eventcard(props:Card) {
+    return (
+        <div className="h-64 w-96 bg-gray-300 rounded-2xl flex flex-col gap-1 px-5 py-4 font-sans tracking-tight shadow-xl hover:scale-105 duration-200">
+            <div className="flex items-center h-10 gap-2 text-nupurple font-bold text-3xl">
+                <Image 
+                    src={props.image}
+                    alt='Event Image'
+                    width={30}
+                    height={30}
+                    id={props.id}
+                />
+                {props.title}
+            </div>
+            <div className="text-nulight text-lg leading-snug">
+                {props.text}
+            </div>
+        </div>
+    );
+}
--- components/events.tsx
+++ components/events.tsx
@@ -1,5 +1,36 @@
+import Eventcard from "./eventcard";
+
 export default function Events() {
     return (
-        <div className="h-full w-full" id="events"></div>
+        <div className="h-full w-full flex items-center justify-center flex-col gap-10 pt-12" id="events">
+            <div className="flex items-center justify-center gap-10">
+                <Eventcard
+                    title={"Discussions"}
+                    text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate"}
+                    image={"/discuss.svg"}
+                    id={"discuss"}
+                />
+                <Eventcard
+                    title={"Guest Speakers"}
+                    text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate"}
+                    image={"/speakers.svg"}
+                    id={"speakers"}
+                />
+            </div>
+            <div className="flex items-center justify-center gap-10">
+                <Eventcard
+                    title={"Competitions"}
+                    text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate"}
+                    image={"/competitions.svg"}
+                    id={"competitions"}
+                />
+                <Eventcard
+                    title={"Hackathons"}
+                    text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate"}
+                    image={"/code.svg"}
+                    id={"code"}
+                />
+            </div>
+        </div>
     );
 }
