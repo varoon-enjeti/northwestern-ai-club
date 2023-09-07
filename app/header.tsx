f6fbf8fb0686203caa8771f03dc2b25e8a18a166
varoon-enjeti <varoon.enjeti@gmail.com>
2023-09-07 18:29:13 +0200

    xl viewport

--- app/header.tsx
+++ app/header.tsx
@@ -22,7 +22,7 @@ export default function Header() {
     return (
        <header className="h-16 w-full fixed flex flex-row text-center bg-gray-300" id="header">
             {scrolled ? 
-            (<div className="flex flex-row text-center gap-[50vw]">
+            (<div className="flex flex-row text-center xl:gap-[50vw] 2xl:gap-[65vw]">
                 <div className='flex flex-row gap-10 text-center items-center'>
                     <Link href="/" className='mr-10'>
                         <Image src="/nuai.png" width={1000} height={1000} alt='' className='h-16 w-16'/>
@@ -45,7 +45,7 @@ export default function Header() {
                 </div>  
             </div>)
             :
-            (<div className="flex flex-row text-center gap-96">
+            (<div className="flex flex-row text-center xl:gap-96 2xl:gap-[30vw]">
                 <div className='flex flex-row gap-10 text-center items-center'>
                     <Link href="/" className='mr-10'>
                         <Image src="/nuai.png" width={1000} height={1000} alt='' className='h-16 w-16'/>
