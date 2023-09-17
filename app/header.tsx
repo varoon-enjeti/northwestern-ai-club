67ed040b704c27dfcbd2c7470b1e0935a54bea6e
varoon-enjeti <varoon.enjeti@gmail.com>
2023-09-17 11:39:17 -0500

    escaping

--- app/header.tsx
+++ app/header.tsx
@@ -19,53 +19,65 @@ export default function Header() {
 
     }, []);
 
-    return (
-       <header className="h-16 w-full fixed flex flex-row text-center sm:bg-gray-300 bg-red-500 z-30" id="header">
-            {scrolled ? 
-            (<div className="flex flex-row text-center xl:gap-[50vw] 2xl:gap-[65vw]">
-                <div className='flex flex-row gap-10 text-center items-center'>
-                    <Link href="/" className='mr-10'>
-                        <Image src="/nuai.png" width={1000} height={1000} alt='' className='h-16 w-16'/>
-                    </Link>
-                    <Link href="#about" className='font-sans text-nupurple text-lg hover:text-nulight'>About</Link>
-                    <Link href="#events" className='font-sans text-nupurple text-lg hover:text-nulight'>Events</Link>
-                    <Link href="#team" className='font-sans text-nupurple text-lg hover:text-nulight'>Team</Link>
-                    <Link href="#join" className='font-sans text-white text-lg bg-nupurple w-20 h-8 flex items-center justify-center animate-[loadOne_1.25s_ease] rounded-lg hover:scale-105 duration-200'>Sign Up</Link>
-                </div>
-                <div className='flex flex-row items-center gap-6'>
-                    <Link href="https://www.instagram.com/northwestern_ai/" target="_blank" className='animate-[loadOne_1.25s_ease]'>
-                        <Image src="/instagram.svg" width={1000} height={1000} alt='' className='h-6 w-6'/>
-                    </Link>
-                    <Link href="https://www.linkedin.com/groups/14288307/" target="_blank" className='animate-[loadOne_1.25s_ease]'>
-                        <Image src="/linkedin.svg" width={1000} height={1000} alt='' className='h-6 w-6'/>
-                    </Link>
-                    <Link href="mailto:northwesternaiclub@gmail.com?subject=I'm Interested in AI Club!" target="_blank" className='animate-[loadOne_1.25s_ease]'>
-                        <Image src="/mail.svg" width={1000} height={1000} alt='' className='h-7 w-7'/>
-                    </Link>
-                </div>  
-            </div>)
-            :
-            (<div className="flex flex-row text-center xl:gap-96 2xl:gap-[30vw]">
-                <div className='flex flex-row gap-10 text-center items-center'>
-                    <Link href="/" className='mr-10'>
-                        <Image src="/nuai.png" width={1000} height={1000} alt='' className='h-16 w-16'/>
-                    </Link>
-                    <Link href="#about" className='font-sans text-nupurple text-lg hover:text-nulight'>About</Link>
-                    <Link href="#events" className='font-sans text-nupurple text-lg hover:text-nulight'>Events</Link>
-                    <Link href="#team" className='font-sans text-nupurple text-lg hover:text-nulight'>Team</Link>
-                </div>
-                <div className='flex flex-row items-center gap-6'>
-                    <Link href="https://www.instagram.com/northwestern_ai/" target="_blank" key={1} className='animate-[loadOne_1.25s_ease]'>
-                        <Image src="/instagram.svg" width={1000} height={1000} alt='' className='h-6 w-6'/>
-                    </Link>
-                    <Link href="https://www.linkedin.com/groups/14288307/" target="_blank" key={2} className='animate-[loadOne_1.25s_ease]'>
-                        <Image src="/linkedin.svg" width={1000} height={1000} alt='' className='h-6 w-6'/>
-                    </Link>
-                    <Link href="mailto:northwesternaiclub@gmail.com?subject=I'm Interested in AI Club!" target="_blank" key={3} className='animate-[loadOne_1.25s_ease]'>
-                        <Image src="/mail.svg" width={1000} height={1000} alt='' className='h-7 w-7'/>
-                    </Link>
-                </div>
-            </div>)}
-       </header>
-    );
+    const [width, setWidth] = useState(window.innerWidth);
+    const breakpoint = 514;
+    useEffect(() => {
+        window.addEventListener("resize", () => setWidth(window.innerWidth));
+    }, []);
+
+    if (width > breakpoint) {
+        return (
+        <header className="lg:h-16 w-full fixed flex flex-row text-center bg-gray-300 z-30" id="header">
+                {scrolled ? 
+                (<div className="flex flex-row text-center xl:gap-[50vw] 2xl:gap-[65vw]">
+                    <div className='flex flex-row gap-10 text-center items-center'>
+                        <Link href="/" className='mr-10'>
+                            <Image src="/nuai.png" width={1000} height={1000} alt='' className='h-16 w-16'/>
+                        </Link>
+                        <Link href="#about" className='font-sans text-nupurple text-lg hover:text-nulight'>About</Link>
+                        <Link href="#events" className='font-sans text-nupurple text-lg hover:text-nulight'>Events</Link>
+                        <Link href="#team" className='font-sans text-nupurple text-lg hover:text-nulight'>Team</Link>
+                        <Link href="#join" className='font-sans text-white text-lg bg-nupurple w-20 h-8 flex items-center justify-center animate-[loadOne_1.25s_ease] rounded-lg hover:scale-105 duration-200'>Sign Up</Link>
+                    </div>
+                    <div className='flex flex-row items-center gap-6'>
+                        <Link href="https://www.instagram.com/northwestern_ai/" target="_blank" className='animate-[loadOne_1.25s_ease]'>
+                            <Image src="/instagram.svg" width={1000} height={1000} alt='' className='h-6 w-6'/>
+                        </Link>
+                        <Link href="https://www.linkedin.com/groups/14288307/" target="_blank" className='animate-[loadOne_1.25s_ease]'>
+                            <Image src="/linkedin.svg" width={1000} height={1000} alt='' className='h-6 w-6'/>
+                        </Link>
+                        <Link href="mailto:northwesternaiclub@gmail.com?subject=I'm Interested in AI Club!" target="_blank" className='animate-[loadOne_1.25s_ease]'>
+                            <Image src="/mail.svg" width={1000} height={1000} alt='' className='h-7 w-7'/>
+                        </Link>
+                    </div>  
+                </div>)
+                :
+                (<div className="flex flex-row text-center xl:gap-96 2xl:gap-[30vw]">
+                    <div className='flex flex-row gap-10 text-center items-center'>
+                        <Link href="/" className='mr-10'>
+                            <Image src="/nuai.png" width={1000} height={1000} alt='' className='h-16 w-16'/>
+                        </Link>
+                        <Link href="#about" className='font-sans text-nupurple text-lg hover:text-nulight'>About</Link>
+                        <Link href="#events" className='font-sans text-nupurple text-lg hover:text-nulight'>Events</Link>
+                        <Link href="#team" className='font-sans text-nupurple text-lg hover:text-nulight'>Team</Link>
+                    </div>
+                    <div className='flex flex-row items-center gap-6'>
+                        <Link href="https://www.instagram.com/northwestern_ai/" target="_blank" key={1} className='animate-[loadOne_1.25s_ease]'>
+                            <Image src="/instagram.svg" width={1000} height={1000} alt='' className='h-6 w-6'/>
+                        </Link>
+                        <Link href="https://www.linkedin.com/groups/14288307/" target="_blank" key={2} className='animate-[loadOne_1.25s_ease]'>
+                            <Image src="/linkedin.svg" width={1000} height={1000} alt='' className='h-6 w-6'/>
+                        </Link>
+                        <Link href="mailto:northwesternaiclub@gmail.com?subject=I'm Interested in AI Club!" target="_blank" key={3} className='animate-[loadOne_1.25s_ease]'>
+                            <Image src="/mail.svg" width={1000} height={1000} alt='' className='h-7 w-7'/>
+                        </Link>
+                    </div>
+                </div>)}
+        </header>
+        );
+    } else {
+        return (
+            <header className="h-12 w-full fixed flex flex-row text-center bg-gray-300 z-30"></header>
+        );
+    }
 }
--- components/home.tsx
+++ components/home.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
     return (
         <div className="w-full h-full" id="home">
-            <div className="h-full w-full flex justify-center overflow-hidden pl-20 items-center bg-gradient-to-b from-white via-transparent via-40% to-white">
+            <div className="h-full w-full flex justify-center overflow-hidden pl-10 lg:pl-20 items-center bg-gradient-to-b from-white via-transparent via-40% to-white">
                 <div className="flex flex-col gap-3">
-                    <p className='h-24 -ml-1 -mb-3 text-8xl tracking-tight font-sans font-bold animate-[loadOne_2.25s_ease] text-transparent bg-clip-text bg-gradient-to-b from-nupurple via-nupurple via-65%' id="home-title">Northwestern AI</p>
+                    <p className='h-24 -ml-1 -mb-3 text-4xl lg:text-8xl tracking-tight font-sans font-bold animate-[loadOne_2.25s_ease] text-transparent bg-clip-text bg-gradient-to-b from-nupurple via-nupurple via-65%' id="home-title">Northwestern AI</p>
                     <p className="w-2/3 text-2xl font-sans font-normal tracking-tight animate-[loadTwo_2.25s_ease] text-nulight">Educating and Empowering Students to Shape the Future with Artificial Intelligence.</p>
                     <a href="#join" className='w-1/6 h-10 flex items-center justify-center font-sans tracking-tight animate-[loadThree_2.25s_ease] font-medium text-xl bg-nupurple rounded-lg text-white hover:scale-105 duration-200'>Sign Up</a>
                 </div>
-                <iframe className='-mr-80 w-[52%] h-[135%] ml-auto rotate-[-17deg] z-40' src="https://particles-js-gamma.vercel.app"></iframe>
+                <iframe className='-mr-36 lg:-mr-80 w-[52%] h-[135%] ml-auto rotate-[-17deg] z-40' src="https://particles-js-gamma.vercel.app"></iframe>
             </div>
         </div>
     );
