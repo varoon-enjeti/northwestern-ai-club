0a6785f11134b84c2cc685bb6d6b6fa1679cd709
varoon-enjeti <varoon.enjeti@gmail.com>
2023-09-18 11:55:24 -0500

    wid

--- components/eventcard.tsx
+++ components/eventcard.tsx
@@ -9,7 +9,7 @@ interface Card {
 
 export default function Eventcard(props:Card) {
     return (
-        <div className="h-[164px] lg:h-64 2xl:h-76 w-11/12 lg:w-1/3 bg-gray-300 rounded-2xl flex flex-col lg:gap-1 px-3 py-2 lg:px-5 lg:py-4 font-sans tracking-tight shadow-xl hover:scale-105 duration-200">
+        <div className="h-[164px] lg:h-64 2xl:h-76 w-10/12 lg:w-1/3 bg-gray-300 rounded-2xl flex flex-col lg:gap-1 px-3 py-2 lg:px-5 lg:py-4 font-sans tracking-tight shadow-xl hover:scale-105 duration-200">
             <div className="flex items-center h-10 gap-1 lg:gap-2 text-nupurple font-bold text-xl lg:text-3xl">
                 <Image 
                     src={props.image}
