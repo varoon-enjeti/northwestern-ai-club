b9fd8d1a7a9f2c469fa1e50de715743167767f76
varoon-enjeti <varoon.enjeti@gmail.com>
2023-09-19 13:44:38 -0500

    team resize

--- components/team.tsx
+++ components/team.tsx
@@ -55,40 +55,40 @@ export default function Team() {
         return (
             <div className="w-full h-screen flex flex-col items-center justify-center gap-10" id="team">
                 <p className="font-sans font-bold text-3xl lg:text-5xl text-nupurple tracking-tight mt-12">Meet the Team!</p>
-                <div className="flex flex-row gap-20">
+                <div className="flex flex-row gap-10 lg:gap-20">
                     <div className="flex flex-col items-center">
                         <a href="https://www.linkedin.com/in/miller-isaac/" className="hover:scale-105 duration-200" target="_blank">
-                            <Image src="/isaac.jpeg" width={1000} height={1000} alt='' className='h-48 w-48 object-cover rounded-full border-4 border-gray-400'/>
+                            <Image src="/isaac.jpeg" width={1000} height={1000} alt='' className='w-36 h-36 lg:h-48 lg:w-48 object-cover rounded-full border-[3px] lg:border-4 border-gray-400'/>
                         </a>
                         <p className="font-sans font-medium text-lg tracking-tight mt-3">Isaac Miller</p>
                         <p className="font-sans font-light tracking-tight">President</p>
                     </div>
                     <div className="flex flex-col items-center">
                         <a href="https://www.linkedin.com/in/jameskuntz/" className="hover:scale-105 duration-200" target="_blank">
-                            <Image src="/james.jpeg" width={1000} height={1000} alt='' className='h-48 w-48 object-cover rounded-full border-4 border-gray-400'/>
+                            <Image src="/james.jpeg" width={1000} height={1000} alt='' className='w-36 h-36 lg:h-48 lg:w-48 object-cover rounded-full border-[3px] lg:border-4 border-gray-400'/>
                         </a>
                         <p className="font-sans font-medium text-lg tracking-tight mt-3">James Kuntz</p>
                         <p className="font-sans font-light tracking-tight">Vice President</p>
                     </div>
                 </div>
-                <div className="flex flex-row gap-20">
+                <div className="flex flex-row gap-10 lg:gap-20">
                     <div className="flex flex-col items-center">
                         <a href="https://www.linkedin.com/in/gracedowning2024/" className="hover:scale-105 duration-200" target="_blank">
-                            <Image src="/grace.jpeg" width={1000} height={1000} alt='' className='h-48 w-48 object-cover rounded-full border-4 border-gray-400'/>
+                            <Image src="/grace.jpeg" width={1000} height={1000} alt='' className='w-36 h-36 lg:h-48 lg:w-48 object-cover rounded-full border-[3px] lg:border-4 border-gray-400'/>
                         </a>
                         <p className="font-sans font-medium text-lg tracking-tight mt-3">Grace Downing</p>
                         <p className="font-sans font-light tracking-tight">Executive Member</p>
                     </div>
                     <div className="flex flex-col items-center">
                         <a href="https://www.linkedin.com/in/helena-guajardo-298880226/" className="hover:scale-105 duration-200" target="_blank">
-                            <Image src="/helena.jpeg" width={1000} height={1000} alt='' className='h-48 w-48 object-cover rounded-full border-4 border-gray-400'/>
+                            <Image src="/helena.jpeg" width={1000} height={1000} alt='' className='w-36 h-36 lg:h-48 lg:w-48 object-cover rounded-full border-[3px] lg:border-4 border-gray-400'/>
                         </a>
                         <p className="font-sans font-medium text-lg tracking-tight mt-3">Helena Guajardo</p>
                         <p className="font-sans font-light tracking-tight">Executive Member</p>
                     </div>
                     <div className="flex flex-col items-center">
                         <a href="https://www.linkedin.com/in/varoonenjeti/" className="hover:scale-105 duration-200" target="_blank">
-                            <Image src="/varoon.png" width={1000} height={1000} alt='' className='h-48 w-48 object-cover rounded-full border-4 border-gray-400'/>
+                            <Image src="/varoon.png" width={1000} height={1000} alt='' className='w-36 h-36 lg:h-48 lg:w-48 object-cover rounded-full border-[3px] lg:border-4 border-gray-400'/>
                         </a>
                         <p className="font-sans font-medium text-lg tracking-tight mt-3">Varoon Enjeti</p>
                         <p className="font-sans font-light tracking-tight">Executive Member</p>
